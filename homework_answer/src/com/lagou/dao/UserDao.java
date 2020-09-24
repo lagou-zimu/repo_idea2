25fdc92f75aff5211dcd9a9a182d8bf13447dabe
lagou-zimu <sunzhichao0713@163.com>
2020-09-24 21:59:31 +0800

    Update UserDao.java

--- homework_answer/src/com/lagou/dao/UserDao.java
+++ homework_answer/src/com/lagou/dao/UserDao.java
@@ -30,6 +30,41 @@ public interface UserDao {
         用户添加
      */
     public void addUser4(User user) throws SQLException;
+    
+    
+    
+        /*
+用户添加
+*/
+    public void aa(User user) throws SQLException;
+
+    /*
+    用户添加
+ */
+    public void bb(User user) throws SQLException;
+
+    /*
+    用户添加
+ */
+    public void cc(User user) throws SQLException;
+
+    /*
+     用户添加
+  */
+    public void dd(User user) throws SQLException;
+
+
+    /*
+     用户添加
+  */
+    public void ee(User user) throws SQLException;
+
+
+    /*
+     用户添加
+  */
+    public void gg(User user) throws SQLException;
+
 
 
 
